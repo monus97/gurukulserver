6a6e7a07cff5586c65399ebefab617c45edb839d
monus97 <monus3871@gmail.com>
2023-12-30 22:00:01 +0530

    worked on cors

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ require("./model/config");
 const express = require("express");
 const app = express();
 const router = require("./routes/userRouter");
+const cors = require('cors');
 const bodyParser = require("body-parser");
-app.use(bodyParser.json())
-app.use(express.json())
- app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+ app.use(bodyParser.urlencoded({extended:true}));
 app.use("/", router);
 
 app.listen(process.env.PORT, (req, res) => {
